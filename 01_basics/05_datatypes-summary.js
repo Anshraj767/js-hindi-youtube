6b1ca347a2fd4240d15410ce759570700149bb8b
Anshraj767 <149093020+Anshraj767@users.noreply.github.com>
2025-06-13 08:01:02 +0000

    datatypes done

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types: Stirng, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bignumber = 232453554535353n
+
+// Reference (non primitive): Arrays, Objects, Functions
+
+
+const cars = ["BMW","AUDI",MAHINDRA,"TATA",] //ARRAY
+
+
+let myObj = {
+    name: "ANSH",
+    age: 20
+}
+
+const myFunction = function(){
+    console.log("This is intro to function");
+    
+}
+
+console.log(typeof bignumber);
+
+
